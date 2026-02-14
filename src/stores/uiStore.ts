db9bf1d2edebda35fdd470a4e83aa678413a380c
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-02-14 12:27:49 +0200

    feat: Add patrol and telemetry detail views to the UI store and extend the Patrol API interface with new fields.

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -3,15 +3,53 @@ import { create } from 'zustand';
 interface UIState {
   violationDetailId: string | null;
   detectionDetailId: string | null;
+  patrolDetailId: string | null;
+  telemetryPatrolId: string | null;
   openViolationDetail: (id: string) => void;
   openDetectionDetail: (id: string) => void;
+  openPatrolDetail: (id: string) => void;
+  openTelemetry: (id: string) => void;
   closeDetail: () => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
   violationDetailId: null,
   detectionDetailId: null,
-  openViolationDetail: (id) => set({ violationDetailId: id, detectionDetailId: null }),
-  openDetectionDetail: (id) => set({ detectionDetailId: id, violationDetailId: null }),
-  closeDetail: () => set({ violationDetailId: null, detectionDetailId: null }),
+  patrolDetailId: null,
+  telemetryPatrolId: null,
+  openViolationDetail: (id) =>
+    set({
+      violationDetailId: id,
+      detectionDetailId: null,
+      patrolDetailId: null,
+      telemetryPatrolId: null,
+    }),
+  openDetectionDetail: (id) =>
+    set({
+      detectionDetailId: id,
+      violationDetailId: null,
+      patrolDetailId: null,
+      telemetryPatrolId: null,
+    }),
+  openPatrolDetail: (id) =>
+    set({
+      patrolDetailId: id,
+      violationDetailId: null,
+      detectionDetailId: null,
+      telemetryPatrolId: null,
+    }),
+  openTelemetry: (id) =>
+    set({
+      telemetryPatrolId: id,
+      violationDetailId: null,
+      detectionDetailId: null,
+      patrolDetailId: null,
+    }),
+  closeDetail: () =>
+    set({
+      violationDetailId: null,
+      detectionDetailId: null,
+      patrolDetailId: null,
+      telemetryPatrolId: null,
+    }),
 }));
